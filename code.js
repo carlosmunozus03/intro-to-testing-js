1a59664811ca00ef5dcca3764f23ba331a719ffa
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-03-24 18:52:15 -0500

    added "addOne" example

--- code.js
+++ code.js
@@ -2,3 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+// addOne
+function addOne(input) {
+    if (typeof input !== "number") {
+        return NaN;
+    } else {
+        return input + 1;
+    }
+}
